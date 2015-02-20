2a79cd43f12baebd8b801345c6f0237dc63c61d6
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-02-20 08:28:49 +0100

    Adding getAPIfromScope() that parses a scope. Also clean up code in this class

--- js/lib/models/APIGK.js
+++ js/lib/models/APIGK.js
@@ -22,23 +22,27 @@ define(function(require, exports, module) {
 			}
 
 			this._super(props);
-
 		},
+
+		/**
+		 * Returns the scope representing basic access for this API Gatekeeper.
+		 * 
+		 * @return {[type]} Scope, like this `gk_foodleapi`
+		 */
 		"getBasicScope": function() {
 			return "gk_" + this.id;
 		},
+
 		"getView": function() {
 			var res = this._super();
 
 			if (this.created) {
-
 				res.created = parseDate(this.created);
 				res.createdAgo = res.created.fromNow();
 				res.createdH = res.created.format('D. MMM YYYY');
 			}
 
 			if  (this.updated) {
-
 				res.updated = parseDate(this.updated);
 				res.updatedAgo = res.updated.fromNow();
 				res.updatedH = res.updated.format('D. MMM YYYY');
@@ -50,11 +54,18 @@ define(function(require, exports, module) {
 				res.scopes = [];
 			}
 
-
 			return res;			
 		}
 
 	});
+
+	// --- Static functions: ----
+
+	APIGK.getAPIfromScope = function(scope) {
+		var match = scope.match(/^gk_([a-z0-9\-]+)(_([a-z0-9\-]+))?$/);
+		if (match !== null) return match[1];
+		return null;
+	};
 
 	return APIGK;
 
