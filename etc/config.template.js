3f551bbc782ad8d3f94288cee91988e70a5e3e8f
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-02-21 18:02:48 +0100

    Make the config template more generic

--- etc/config.template.js
+++ etc/config.template.js
@@ -1,8 +1,8 @@
 define(function(require, exports, module) {
 
 	return {
-		"client_id": "e8160a77-58f8-4006-8ee5-ab64d17a5b1e",
-		"redirect_uri": "http://developers.dev.feideconnect.no/index.dev.html",
+		"client_id": "xxxxx",
+		"redirect_uri": "http://xxxxx.example.org/",
 		"debug": true,
 		"instance": "dev"
 	};
