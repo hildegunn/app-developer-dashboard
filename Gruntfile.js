09cd367ddba98151079a8ce59f2d057c17e5601d
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-02-21 07:30:18 +0100

    Making separate CI version of bower.json that points to public version of uninett-theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,15 +11,6 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		// uglify: {
-		// 	options: {
-		// 		banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-		// 	},
-		// 	build: {
-		// 		src: 'src/<%= pkg.name %>.js',
-		// 		dest: 'build/<%= pkg.name %>.min.js'
-		// 	}
-		// },
 	    shell: {
 	        rcss: {
 				command: 'r.js -o build.css.js'
@@ -34,56 +25,7 @@ module.exports = function(grunt) {
 		watch: {
 			files: ['<%= jshint.files %>'],
 			tasks: ['jshint']
-		},
-		requirejs: {
-			compile: {
-				options: {
-					baseUrl: "js",
-					mainConfigFile: "build.js",
-					paths: {
-						"bower"     : '../bower_components',
-						"text"      : '../bower_components/text/text',
-						"templates" : '../templates/',
-						"dust"      : '../bower_components/dustjs-linkedin/dist/dust-full.min',
-						"class"     : "lib/class",
-					},
-					shim: {
-						"dust": {
-							"exports": "dust"
-						}
-					},
-					name: "../bower_components/almond/almond",
-					include: "main-dev",
-					out: "dist/app.min.js"
-				}
-			}
 		}
-		// requirejs: {
-		// 	compile: {
-		// 		options: {
-		// 			almond: true,
-		// 			// dir: 'build',
-		// 			// appDir: '.',
-		// 			baseUrl: 'js',
-		// 			name: "main-dev",
-		// 			out: "build/app.min.js",
-		// 			// include: ['main-dev'],
-		// 			optimize: "none",
-		// 			paths: {
-		// 				"bower"     : '../bower_components',
-		// 				"text"      : '../bower_components/text/text',
-		// 				"templates" : '../templates/',
-		// 				"dust"      : '../bower_components/dustjs-linkedin/dist/dust-full.min',
-		// 				"class"     : "class"
-		// 			},
-		// 			wrap: {
-		// 				startFile: 'etc/start.frag',
-		// 				endFile: 'etc/end.frag'
-		// 			},
-		// 			preserveLicenseComments: false
-		// 		}
-		// 	}
-		// }
 	});
 
 
@@ -92,7 +34,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-shell');
 	
 
-	// Default task(s).
+	// Tasks
 	grunt.registerTask('default', ['jshint']);
 	grunt.registerTask('buildx', ['requirejs']);
 	grunt.registerTask('bower', ['shell:bower']);
