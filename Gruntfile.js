e9bd13f13b34517c895bef8026045e857283adbb
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-02-24 15:45:07 +0100

    Added grunt task to download translated strings from transifex

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,17 @@ module.exports = function(grunt) {
 		watch: {
 			files: ['<%= jshint.files %>'],
 			tasks: ['jshint']
+		},
+		transifex: {
+			"feide-connect": {
+				options: {
+					targetDir: "./dictionaries/transifex",         // download specified resources / langs only
+					resources: ["developer-dashboard"],
+					languages: ["en_US", "fr"],
+					filename : "dictionary._lang_.json"
+					// templateFn: function(strings) { return strings; }  // customize the output file format (see below)
+				}
+			}
 		}
 	});
 
@@ -37,22 +48,34 @@ module.exports = function(grunt) {
 	shell.rjs.command = [];
 	for(var i = 0; i < cfg.languages.length; i++) {
 		lang = cfg.languages[i];
-		shell.rjs.command.push("node_modules/requirejs/bin/r.js -o build.js paths.dict=../dictionaries/dictionary." + lang + ".json out=dist/app.min.js." + lang + "");
+		shell.rjs.command.push("node_modules/requirejs/bin/r.js -o build.js paths.dict=../dictionaries/transifex/dictionary." + lang + ".json out=dist/app.min.js." + lang + "");
 	}
 	shell.rjs.command.push("cp dist/app.min.js.en app.min.js");
+
+	for(var to in cfg["language-aliases"]) {
+		var tofile = "dist/app.min.js." + to;
+		var fromfile = "dist/app.min.js." + cfg["language-aliases"][to];
+		shell.rjs.command.push("cp " + fromfile + " " + tofile);
+	}
+
 	shell.rjs.command = shell.rjs.command.join(" && ");
 	grunt.config("shell", shell);
 
+	var transifex = grunt.config.get("transifex");
+	transifex["feide-connect"].options.languages = cfg.languages;
+	grunt.config.set("transifex", transifex);
 
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-requirejs');
 	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-transifex');
 	
-
 	// Tasks
 	grunt.registerTask('default', ['jshint']);
 	grunt.registerTask('bower', ['shell:bower']);
-	grunt.registerTask('build', ['shell:bower', 'jshint', 'shell:rcss', 'shell:rjs']);
+	grunt.registerTask('build', ['shell:bower', 'jshint', 'shell:rcss', 'lang', 'shell:rjs']);
 	grunt.registerTask('test', ['jshint']);
+
+	grunt.registerTask('lang', ['transifex']);
 };
--- etc/config.template.js
+++ etc/config.template.js
@@ -2,5 +2,10 @@
 	"client_id": "xxxxx",
 	"redirect_uri": "http://xxxxx.example.org/",
 	"debug": true,
-	"instance": "dev"
+	"instance": "dev",
+
+	"languages": ["en", "nb"],
+	"language-aliases": {
+		"no": "nb"
+	}
 }
