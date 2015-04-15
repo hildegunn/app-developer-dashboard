b2cb9568e9d7ae8a757e6c892a19767ab1d02ba5
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-04-15 20:01:42 +0200

    add use strict

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
-"use strict";	
-
 /* jshint node: true */
 module.exports = function(grunt) {
 	
+	"use strict";	
+
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
