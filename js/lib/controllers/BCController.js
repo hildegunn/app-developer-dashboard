81e5e2896c03b01958b65046bc30cd05a99692c3
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-10-19 08:31:35 +0200

    Remove unused code

--- js/lib/controllers/BCController.js
+++ js/lib/controllers/BCController.js
@@ -20,32 +20,6 @@ define(function(require, exports, module) {
 			this.tmp = new TemplateEngine(template);
 		},
 
-		"initLoad": function() {
-
-			var that = this;
-
-			return that.feideconnect.vootGroupsList()
-				.then(function(groups) {
-
-					for(var i = 0; i < groups.length; i++) {
-
-						var g = new Group(groups[i]);
-
-						// Only use group memberships where a user is admin in an orgadmin group.
-						if (!g.isType("fc:orgadmin")) { continue; }
-						if (!g.isMemberType("admin")) { continue; }
-
-						// console.error("GROUP ", g);
-						that.enabled = true;
-						that.roles[groups[i].org] = new GroupOption({"group": g});
-					}
-
-				})
-				.then(this.proxy("draw"))
-				.then(that.proxy("_initLoaded"));
-
-		},
-
 		"draw": function(items) {
 
 
