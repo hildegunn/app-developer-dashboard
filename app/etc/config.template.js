fccdcb849f5c4e68f6b854e6b88ca5d991d7a131
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-11-04 11:15:05 +0100

    Update config to match production

--- app/etc/config.template.js
+++ app/etc/config.template.js
@@ -1,9 +1,10 @@
 {
-	"client_id": "xxxxx",
-	"redirect_uri": "http://xxxxx.example.org/",
+	"client_id": "a76c907f-1922-456c-a037-1b30af82c7be",
+	"redirect_uri": "https://dashboard.feideconnect.no/",
 	"debug": true,
-	"instance": "dev",
+	"instance": "pilot",
 
+	"cookiedomain": ".feideconnect.no",
 	"languages": ["en", "nb"],
 	"language-aliases": {
 		"no": "nb"
