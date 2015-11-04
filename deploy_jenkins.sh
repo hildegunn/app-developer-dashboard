68bbc47f519a3544fcfcd2f654763d264401aecf
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-11-04 11:15:16 +0100

    Add jenkins build script

---
+++ deploy_jenkins.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+#app=$(jq -r .name nova.config.json)
+#
+app=dashboard
+hostnameapp=dashboard
+oldapp="${app}-old"
+
+
+
+domain="feideconnect.no"
+#domain="paas.uninett.no"
+
+echo "Ready to deploy updated version of ${domain}"
+
+
+. ~/cf-login.sh
+cf target -o system -s prod
+
+cp app/etc/config.template.js app/etc/config.js
+
+npm install
+#npm prepublish
+
+if cf app "${app}" |egrep -q '#.*running'
+then
+    first='n'
+    if cf app "${oldapp}"
+    then
+        cf delete -f "${oldapp}"
+    fi
+    cf rename "${app}" "${oldapp}"
+else
+    first='y'
+fi
+cf push "${app}" -k 384M -m 128M -i 2
+cf map-route "${app}" "${domain}" -n "${hostnameapp}"
+
+if [ "${first}" = 'n' ]
+then
+    cf unmap-route "${oldapp}" "${domain}"
+    cf stop "${oldapp}"
+fi
+
+echo "Done."
