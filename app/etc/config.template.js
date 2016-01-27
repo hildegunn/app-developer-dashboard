97d7c08c75fb411f2cf42e73a624b4751ae011b9
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2016-01-27 16:42:50 +0100

    Set redirect uri for production

--- app/etc/config.template.js
+++ app/etc/config.template.js
@@ -1,6 +1,6 @@
 {
     "client_id": "a76c907f-1922-456c-a037-1b30af82c7be",
-    "redirect_uri": "https://dashboard.feideconnect.no/",
+    "redirect_uri": "https://dashboard.dataporten.no/",
     "debug": false,
     "instance": "pilot",
 
